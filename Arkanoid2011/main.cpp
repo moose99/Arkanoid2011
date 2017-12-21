117bebb9edebe68664cee2f4c4ded80de882e479
mus <mus@thamers.com>
2017-12-21 17:47:18 -0500

    major refactoring using manager class and entity objects using modern C++

--- Arkanoid2011/main.cpp
+++ Arkanoid2011/main.cpp
@@ -2,13 +2,184 @@
 // License: MIT License | http://opensource.org/licenses/MIT
 // http://vittorioromeo.info | vittorio.romeo@outlook.com
 
-// In this code segment we'll start improving the game's code
-// architecture. The first step will be creating a `Game` class
-// that will encapsulate the game's state.
+// The next step that will greatly improve our code architecture
+// is creating a system that allows us to quickly create new
+// game object types during development, and also instantiate and
+// destroy game object instances at run-time. We're gonna need
+// a polymorphic hierarchy for our objects, and a manager class
+// that will help us deal with them.
 
+// Let's include the `<memory>` header, as we're going to use
+// smart pointers.
+#include <memory>
+
+// We will also need the `<typeinfo>` header  and the `<map>` header
+// to query entities by type.
+#include <typeinfo>
+#include <map>
 #include <SFML/Graphics.hpp>
 
 constexpr unsigned int wndWidth{ 800 }, wndHeight{ 600 };
+
+// All game objects share the same interface.
+// They provide a `update` and a `draw` method.
+// Let's create a polymorphic hierarchy for our objects.
+// The base class will be called `Entity`.
+// It will also store the `destroyed` bool value, so that
+// we can "mark" dead entities whose memory will be reclaimed.
+class Entity
+{
+public:
+	bool destroyed{ false };
+
+	// We use the `virtual` keyword to enable polymorphism.
+	virtual ~Entity() {}
+	virtual void update() {}
+	virtual void draw(sf::RenderWindow& mTarget) {}
+};
+
+// Now, we need a manager class that will help us create,
+// destroy, update and query entities.
+class Manager
+{
+private:
+	// Since our entities are now polymorphic, we need to store
+	// them on the heap. We'll use a `std::vector` of
+	// `std::unique_ptr<Entity>` to enable polymorphism.
+	std::vector<std::unique_ptr<Entity>> entities;
+
+	// We also need to get all entities of a certain type
+	// during the game loop. For example, we need to check
+	// if any ball collides with any brick. Instead of manually
+	// checking the type of the entity during the loop, we can
+	// store a "database" of game objects, an `std::map` of
+	// `std::vector` instances where the key is a `typeid` hash.
+	std::map<std::size_t, std::vector<Entity*>> groupedEntities;
+
+public:
+	// To properly populate/query these data structures, we'll need
+	// some methods. We're gonna use C++11 variadic templates to
+	// allow the user to create entities with any constructor signature.
+
+	// The first method we're gonna define is `create`: it will
+	// take a game object type `T`, and some `TArgs` constructor
+	// arguments types as template parameters, and return a reference
+	// to an heap-allocated object. The object itself will be stored
+	// as an `std::unique_ptr` in the `entities` vector, and a pointer
+	// to it will be stored in `groupedEntities` for easy querying.
+	// The key that will be used for the `groupedEntities` storage
+	// will be the type hash of `T` retrieved thanks to `<typeinfo>`.
+	template <typename T, typename... TArgs>
+	T& create(TArgs&&... mArgs)
+	{
+		// Let's make sure, using a `static_assert`, that the type `T`
+		// is a child of the `Entity` type inheritance.
+		static_assert(std::is_base_of<Entity, T>::value,
+			"`T` must be derived from `Entity`");
+
+		// Let's create the object itself, using `std::make_unique`.
+		// We'll use perfect forwarding to make sure the types of the
+		// arguments passed to `T`'s constructor will be forwarded
+		// properly.
+		auto uPtr(std::make_unique<T>(std::forward<TArgs>(mArgs)...));
+		auto ptr(uPtr.get());
+
+		// Let's retrieve the `T` type hash with the `typeid` keyword.
+		// The retrieved hash code is guaranteed to be the same for `T`.
+		// Let's use it as the key for the `groupedEntities` entry.
+		groupedEntities[typeid(T).hash_code()].emplace_back(ptr);
+
+		// [07/10/2014 addendum]: `hash_code()` does not actually guarantee
+		// that the codes generated for two different types will be
+		// unique. Learn more about this issue and a possible solution on
+		// cppreference:
+		// http://en.cppreference.com/w/cpp/types/type_info/hash_code
+
+		// Now let's move the `std::unique_ptr` in the `entities`
+		// vector.
+		entities.emplace_back(std::move(uPtr));
+
+		return *ptr;
+	}
+
+	// Removal of an entity will work in a different way: instead of
+	// directly removing the entity from the storage, we will simply
+	// mark it as "destroyed". Another method, called `refresh`, will
+	// take care of cleaning up all the "destroyed" entities, at the
+	// end of an update. This has major performance advantages, and
+	// also allows us to correctly access a soon-to-be-destroyed entity
+	// without accessing corrupted memory.
+	void refresh()
+	{
+		// This method will take care of cleaning up the destroyed
+		// entities. We begin looking for entities to remove in the
+		// `groupedEntities` storage, so that their content will
+		// still be accessible.
+		for (auto& pair : groupedEntities)
+		{
+			auto& vector(pair.second);
+
+			vector.erase(std::remove_if(std::begin(vector), std::end(vector),
+				[](auto mPtr)
+			{
+				return mPtr->destroyed;
+			}),
+				std::end(vector));
+		}
+
+		// After that, we use the same idiom on the `entities` vector.
+		// Since `entities` stores smart pointers, the memory will be
+		// automatically freed when they are removed from the vector.
+		entities.erase(std::remove_if(std::begin(entities), std::end(entities),
+			[](const auto& mUPtr)
+		{
+			return mUPtr->destroyed;
+		}),
+			std::end(entities));
+	}
+
+	// We'll also need a `clear` method to destroy all entities.
+	void clear()
+	{
+		groupedEntities.clear();
+		entities.clear();
+	}
+
+	// And a template method to query the grouped storage.
+	// A good candidate for C++14's automatic function
+	// return type deduction.
+	template <typename T>
+	auto& getAll()
+	{
+		return groupedEntities[typeid(T).hash_code()];
+	}
+
+	// Another useful method will allow the user to execute arbitrary
+	// code on all entities of a certain type.
+	template <typename T, typename TFunc>
+	void forEach(const TFunc& mFunc)
+	{
+		// Retrieve all entities of type `T`.
+		auto& vector(getAll<T>());
+
+		// For each pointer in the entity vector, simply cast the
+		// pointer to its "real" type then call the function with the
+		// casted pointer, dereferenced.
+		for (auto ptr : vector) mFunc(*reinterpret_cast<T*>(ptr));
+	}
+
+	// Lastly, we'll implement a method to update all entities, and a
+	// method to draw all entities.
+
+	void update()
+	{
+		for (auto& e : entities) e->update();
+	}
+	void draw(sf::RenderWindow& mTarget)
+	{
+		for (auto& e : entities) e->draw(mTarget);
+	}
+};
 
 struct Rectangle
 {
@@ -37,12 +208,13 @@ struct Circle
 	float bottom() const noexcept { return y() + radius(); }
 };
 
-class Ball : public Circle
+// Let's now adapt our classes to the new architecture.
+
+class Ball : public Entity, public Circle
 {
 public:
 	static const sf::Color defColor;
-	static constexpr float defRadius{ 10.f };
-	static constexpr float defVelocity{ 8.f };
+	static constexpr float defRadius{ 10.f }, defVelocity{ 8.f };
 
 	sf::Vector2f velocity{ -defVelocity, -defVelocity };
 
@@ -54,13 +226,16 @@ public:
 		shape.setOrigin(defRadius, defRadius);
 	}
 
-	void update()
+	// The `override` C++11 keyword is incredibly useful.
+	// It makes sure that you're overriding a virtual method
+	// of the base class.
+	void update() override
 	{
 		shape.move(velocity);
 		solveBoundCollisions();
 	}
 
-	void draw(sf::RenderWindow& mTarget) { mTarget.draw(shape); }
+	void draw(sf::RenderWindow& mTarget) override { mTarget.draw(shape); }
 
 private:
 	void solveBoundCollisions() noexcept
@@ -79,12 +254,11 @@ private:
 
 const sf::Color Ball::defColor{ sf::Color::Red };
 
-class Paddle : public Rectangle
+class Paddle : public Entity, public Rectangle
 {
 public:
 	static const sf::Color defColor;
-	static constexpr float defWidth{ 60.f };
-	static constexpr float defHeight{ 20.f };
+	static constexpr float defWidth{ 60.f }, defHeight{ 20.f };
 	static constexpr float defVelocity{ 8.f };
 
 	sf::Vector2f velocity;
@@ -97,13 +271,13 @@ public:
 		shape.setOrigin(defWidth / 2.f, defHeight / 2.f);
 	}
 
-	void update()
+	void update() override
 	{
 		processPlayerInput();
 		shape.move(velocity);
 	}
 
-	void draw(sf::RenderWindow& mTarget) { mTarget.draw(shape); }
+	void draw(sf::RenderWindow& mTarget) override { mTarget.draw(shape); }
 
 private:
 	void processPlayerInput()
@@ -120,15 +294,12 @@ private:
 
 const sf::Color Paddle::defColor{ sf::Color::Red };
 
-class Brick : public Rectangle
+class Brick : public Entity, public Rectangle
 {
 public:
 	static const sf::Color defColor;
-	static constexpr float defWidth{ 60.f };
-	static constexpr float defHeight{ 20.f };
+	static constexpr float defWidth{ 60.f }, defHeight{ 20.f };
 	static constexpr float defVelocity{ 8.f };
-
-	bool destroyed{ false };
 
 	Brick(float mX, float mY)
 	{
@@ -138,8 +309,7 @@ public:
 		shape.setOrigin(defWidth / 2.f, defHeight / 2.f);
 	}
 
-	void update() {}
-	void draw(sf::RenderWindow& mTarget) { mTarget.draw(shape); }
+	void draw(sf::RenderWindow& mTarget) override { mTarget.draw(shape); }
 };
 
 const sf::Color Brick::defColor{ sf::Color::Yellow };
@@ -183,13 +353,9 @@ void solveBrickBallCollision(Brick& mBrick, Ball& mBall) noexcept
 		mBall.velocity.y = ballFromTop ? -Ball::defVelocity : Ball::defVelocity;
 }
 
-// The `Game` class will store game constants and elements, and
-// will keep track of the game's state. Also, it will provide
-// functionality to pause and restart the game.
 class Game
 {
 private:
-	// Let's create an enum for the possible game states.
 	enum class State
 	{
 		Paused,
@@ -200,24 +366,22 @@ private:
 	static constexpr int brkStartColumn{ 1 }, brkStartRow{ 2 };
 	static constexpr float brkSpacing{ 3.f }, brkOffsetX{ 22.f };
 
-	sf::RenderWindow window{ { wndWidth, wndHeight }, "Arkanoid - 9" };
+	sf::RenderWindow window{ { wndWidth, wndHeight }, "Arkanoid - 10" };
 
-	Ball ball{ wndWidth / 2.f, wndHeight / 2.f };
-	Paddle paddle{ wndWidth / 2, wndHeight - 50 };
-	std::vector<Brick> bricks;
+	// The `Game` class will now store our manager.
+	Manager manager;
 
-	// Let's add some fields to keep track of the game status.
 	State state{ State::InProgress };
 	bool pausePressedLastFrame{ false };
 
 public:
 	Game() { window.setFramerateLimit(60); }
 
-	// The `restart` method will re-create all game objects and restore
-	// the game to a default state.
 	void restart()
 	{
+		// Restarting will clear the manager and re-create all entities.
 		state = State::Paused;
+		manager.clear();
 
 		for (int iX{ 0 }; iX < brkCountX; ++iX)
 			for (int iY{ 0 }; iY < brkCountY; ++iY)
@@ -225,14 +389,15 @@ public:
 				float x{ (iX + brkStartColumn) * (Brick::defWidth + brkSpacing) };
 				float y{ (iY + brkStartRow) * (Brick::defHeight + brkSpacing) };
 
-				bricks.emplace_back(brkOffsetX + x, y);
+				// As you can see, creating entities using the manager is
+				// really straightforward.
+				manager.create<Brick>(brkOffsetX + x, y);
 			}
 
-		ball = Ball{ wndWidth / 2.f, wndHeight / 2.f };
-		paddle = Paddle{ wndWidth / 2, wndHeight - 50 };
+		manager.create<Ball>(wndWidth / 2.f, wndHeight / 2.f);
+		manager.create<Paddle>(wndWidth / 2, wndHeight - 50);
 	}
 
-	// The `run` method will start the game loop.
 	void run()
 	{
 		while (true)
@@ -241,13 +406,8 @@ public:
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) break;
 
-			// The `P` key will toggle the pause. To prevent continuous
-			// use of the pause button, we need to check if the input
-			// was pressed last frame.
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::P))
 			{
-				// If `P` was not pressed last frame, we can toggle
-				// the state.
 				if (!pausePressedLastFrame)
 				{
 					if (state == State::Paused)
@@ -255,55 +415,65 @@ public:
 					else if (state == State::InProgress)
 						state = State::Paused;
 				}
-
 				pausePressedLastFrame = true;
 			}
 			else
 				pausePressedLastFrame = false;
 
-			// Let's also use the `R` key to restart the game.
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R)) restart();
 
-			// If the game is paused, we'll only draw game elements,
-			// without updating them.
 			if (state != State::Paused)
 			{
-				// The rest of the game loop code is exactly the same.
+				// Instead of manually updating every entity, we just
+				// ask the manager to do the work for us.
+				manager.update();
 
-				ball.update();
-				paddle.update();
-				for (auto& brick : bricks)
+				// The game logic is now much more generic: we ask the
+				// manager to give us all instances of a certain game
+				// object type, then we run the collision functions.
+				// This is very flexible as we can have any number
+				// of balls and bricks, and adding new types of game
+				// objects is extremely easy.
+				manager.forEach<Ball>([this](auto& mBall)
 				{
-					brick.update();
-					solveBrickBallCollision(brick, ball);
-				}
+					manager.forEach<Brick>([this, &mBall](auto& mBrick)
+					{
+						solveBrickBallCollision(mBrick, mBall);
+					});
+					manager.forEach<Paddle>([this, &mBall](auto& mPaddle)
+					{
+						solvePaddleBallCollision(mPaddle, mBall);
+					});
+				});
 
-				bricks.erase(
-					std::remove_if(std::begin(bricks), std::end(bricks),
-						[](const auto& mBrick)
-				{
-					return mBrick.destroyed;
-				}),
-					std::end(bricks));
-
-				solvePaddleBallCollision(paddle, ball);
+				// Now we ask the manager to clean-up the destroyed
+				// entities.
+				manager.refresh();
 			}
 
-			ball.draw(window);
-			paddle.draw(window);
-			for (auto& brick : bricks) brick.draw(window);
-
+			manager.draw(window);
 			window.display();
 		}
 	}
 };
 
+
 int main()
 {
-	// To start our game, we create a `Game` instance,
-	// restart it and run it.
 	Game game;
 	game.restart();
 	game.run();
 	return 0;
 }
+
+// The code is now much more complex, but the advantages the programmer
+// gets from this kind of design are significant.
+// Using a manager to deal with entities greatly simplifies the addition
+// of new game object types, and also allows the developer to pay less
+// attention to memory-management.
+
+// In the next (and last) code segment, we'll add some completely optional
+// finishing touches to our simple arkanoid clone:
+// * Text
+// * Win/lose states (limited lives)
+// * Multi-hit bricks
