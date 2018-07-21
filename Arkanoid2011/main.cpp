bfbce6cbe30a7f4aa5ad40e57a2ad1864fb80ccf
moose99 <6894921+moose99@users.noreply.github.com>
2018-07-21 00:14:46 -0400

    conversion WIP

--- Arkanoid2011/main.cpp
+++ Arkanoid2011/main.cpp
@@ -2,9 +2,88 @@
 #include <memory>
 #include <typeinfo>
 #include <map>
+#include<string>
+#include <ostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include "SDL.h"
+#include "SDL_ttf.h"
 
-#if 0
-#include <SFML/Graphics.hpp>
+/**
+* Log an SDL error with some error message to the output stream of our choice
+* @param os The output stream to write the message to
+* @param msg The error message to write, format will be msg error: SDL_GetError()
+*/
+void logSDLError( std::ostream &os, const std::string &msg )
+{
+	os << msg << " error: " << SDL_GetError() << std::endl;
+}
+
+/**
+* Render the message we want to display to a texture for drawing
+* @param message The message we want to display
+* @param fontFile The font we want to use to render the text
+* @param color The color we want the text to be
+* @param fontSize The size we want the font to be
+* @param renderer The renderer to load the texture in
+* @return An SDL_Texture containing the rendered message, or nullptr if something went wrong
+*/
+SDL_Texture* renderText( const std::string &message, const std::string &fontFile,
+	SDL_Color color, int fontSize, SDL_Renderer *renderer )
+{
+	//Open the font
+	TTF_Font *font = TTF_OpenFont( fontFile.c_str(), fontSize );
+	if (font == nullptr)
+	{
+		logSDLError( std::cout, "TTF_OpenFont" );
+		return nullptr;
+	}
+	//We need to first render to a surface as that's what TTF_RenderText
+	//returns, then load that surface into a texture
+	SDL_Surface *surf = TTF_RenderText_Blended( font, message.c_str(), color );
+	if (surf == nullptr)
+	{
+		TTF_CloseFont( font );
+		logSDLError( std::cout, "TTF_RenderText" );
+		return nullptr;
+	}
+	SDL_Texture *texture = SDL_CreateTextureFromSurface( renderer, surf );
+	if (texture == nullptr)
+	{
+		logSDLError( std::cout, "CreateTexture" );
+	}
+	//Clean up the surface and font
+	SDL_FreeSurface( surf );
+	TTF_CloseFont( font );
+	return texture;
+}
+
+void fill_circle( SDL_Renderer *renderer, int cx, int cy, int radius, Uint8 r, Uint8 g, Uint8 b, Uint8 a )
+{
+	// Note that there is more to altering the bitrate of this 
+	// method than just changing this value.  See how pixels are
+	// altered at the following web page for tips:
+	//   http://www.libsdl.org/intro.en/usingvideo.html
+	static const int BPP = 4;
+
+	for (double dy = 1; dy <= radius; dy += 1.0)
+	{
+		// This loop is unrolled a bit, only iterating through half of the
+		// height of the circle.  The result is used to draw a scan line and
+		// its mirror image below it.
+
+		// The following formula has been simplified from our original.  We
+		// are using half of the width of the circle because we are provided
+		// with a center and we need left/right coordinates.
+
+		double dx = floor( sqrt( (2.0 * radius * dy) - (dy * dy) ) );
+		int x = cx - dx;
+		SDL_SetRenderDrawColor( renderer, r, g, b, a );
+		SDL_RenderDrawLine( renderer, cx - dx, cy + dy - radius, cx + dx, cy + dy - radius );
+		SDL_RenderDrawLine( renderer, cx - dx, cy - dy + radius, cx + dx, cy - dy + radius );
+	}
+}
 
 constexpr unsigned int wndWidth{ 800 }, wndHeight{ 600 };
 
@@ -15,7 +94,7 @@ public:
 
 	virtual ~Entity() {}
 	virtual void update() {}
-	virtual void draw(sf::RenderWindow& mTarget) {}
+	virtual void draw( SDL_Renderer *renderer) {}
 };
 
 class Manager
@@ -83,62 +162,91 @@ public:
 	{
 		for (auto& e : entities) e->update();
 	}
-	void draw(sf::RenderWindow& mTarget)
+	void draw(SDL_Renderer *renderer)
 	{
-		for (auto& e : entities) e->draw(mTarget);
+		for (auto& e : entities) e->draw(renderer);
 	}
 };
 
-struct Rectangle
+struct Vector2f
 {
-	sf::RectangleShape shape;
-
-	float x() const noexcept { return shape.getPosition().x; }
-	float y() const noexcept { return shape.getPosition().y; }
-	float width() const noexcept { return shape.getSize().x; }
-	float height() const noexcept { return shape.getSize().y; }
-	float left() const noexcept { return x() - width() / 2.f; }
-	float right() const noexcept { return x() + width() / 2.f; }
-	float top() const noexcept { return y() - height() / 2.f; }
-	float bottom() const noexcept { return y() + height() / 2.f; }
+	float x, y;
 };
 
-struct Circle
+struct Shape
 {
-	sf::CircleShape shape;
+	float x, y;
+	float originX, originY;
+	SDL_Color fillColor;
 
-	float x() const noexcept { return shape.getPosition().x; }
-	float y() const noexcept { return shape.getPosition().y; }
-	float radius() const noexcept { return shape.getRadius(); }
-	float left() const noexcept { return x() - radius(); }
-	float right() const noexcept { return x() + radius(); }
-	float top() const noexcept { return y() - radius(); }
-	float bottom() const noexcept { return y() + radius(); }
+	void move( const Vector2f &vel ) { x += vel.x; y += vel.y; }
+	void setPosition( float mX, float mY ) { x = mX; y = mY; }
+	void setFillColor( const SDL_Color &defColor ) { fillColor = defColor; }
+	void setOrigin( float oX, float oY ) { originX = oX; originY = oY; }
+};
+
+struct Rectangle : public Shape
+{
+	float w, h;
+
+	void setSize( float width, float height ) { w = width; h = height; }
+	float width() const noexcept { return w; }
+	float height() const noexcept { return h; }
+	float left() const noexcept { return x - width() / 2.f; }
+	float right() const noexcept { return x + width() / 2.f; }
+	float top() const noexcept { return y - height() / 2.f; }
+	float bottom() const noexcept { return y + height() / 2.f; }
+
+	void drawShape( SDL_Renderer *renderer )
+	{
+		SDL_Rect rect = { x, y, w, h };	// TODO - round
+		SDL_SetRenderDrawColor( renderer, fillColor.r, fillColor.g, fillColor.b, fillColor.a );
+		SDL_RenderFillRect( renderer, &rect );
+	}
+};
+
+// shape info
+struct Circle : public Shape
+{
+	float radius;
+
+	float left() const noexcept { return x - radius; }
+	float right() const noexcept { return x + radius; }
+	float top() const noexcept { return y - radius; }
+	float bottom() const noexcept { return y + radius; }
+	void setRadius( float r ) { radius = r; }
+	void drawShape( SDL_Renderer *renderer )
+	{	// TODO - round
+		fill_circle( renderer, x, y, radius, fillColor.r, fillColor.g, fillColor.b, fillColor.a );
+	}
 };
 
 class Ball : public Entity, public Circle
 {
 public:
-	static const sf::Color defColor;
+	static const SDL_Color defColor;
 	static constexpr float defRadius{ 10.f }, defVelocity{ 8.f };
 
-	sf::Vector2f velocity{ -defVelocity, -defVelocity };
+	Vector2f velocity{ -defVelocity, -defVelocity };
 
 	Ball(float mX, float mY)
 	{
-		shape.setPosition(mX, mY);
-		shape.setRadius(defRadius);
-		shape.setFillColor(defColor);
-		shape.setOrigin(defRadius, defRadius);
+		setPosition(mX, mY);
+		setRadius(defRadius);
+		setFillColor(defColor);
+		setOrigin(defRadius, defRadius);
 	}
 
 	void update() override
 	{
-		shape.move(velocity);
+		move(velocity);
 		solveBoundCollisions();
 	}
 
-	void draw(sf::RenderWindow& mTarget) override { mTarget.draw(shape); }
+	void draw(SDL_Renderer *renderer) override 
+	{
+		drawShape( renderer );
+	}
 
 private:
 	void solveBoundCollisions() noexcept
@@ -159,32 +267,35 @@ private:
 	}
 };
 
-const sf::Color Ball::defColor{ sf::Color::Red };
+const SDL_Color Ball::defColor{ 255,0,0,255 };
 
 class Paddle : public Entity, public Rectangle
 {
 public:
-	static const sf::Color defColor;
+	static const SDL_Color defColor;
 	static constexpr float defWidth{ 60.f }, defHeight{ 20.f };
 	static constexpr float defVelocity{ 8.f };
 
-	sf::Vector2f velocity;
+	Vector2f velocity;
 
 	Paddle(float mX, float mY)
 	{
-		shape.setPosition(mX, mY);
-		shape.setSize({ defWidth, defHeight });
-		shape.setFillColor(defColor);
-		shape.setOrigin(defWidth / 2.f, defHeight / 2.f);
+		setPosition(mX, mY);
+		setSize(defWidth, defHeight );
+		setFillColor(defColor);
+		setOrigin(defWidth / 2.f, defHeight / 2.f);
 	}
 
 	void update() override
 	{
 		processPlayerInput();
-		shape.move(velocity);
+		move(velocity);
 	}
 
-	void draw(sf::RenderWindow& mTarget) override { mTarget.draw(shape); }
+	void draw(SDL_Renderer *renderer) override 
+	{ 
+		drawShape( renderer );
+	}
 
 private:
 	void processPlayerInput()
@@ -199,14 +310,14 @@ private:
 	}
 };
 
-const sf::Color Paddle::defColor{ sf::Color::Red };
+const SDL_Color Paddle::defColor{ 255,0,0,255 };
 
 class Brick : public Entity, public Rectangle
 {
 public:
-	static const sf::Color defColorHits1;
-	static const sf::Color defColorHits2;
-	static const sf::Color defColorHits3;
+	static const SDL_Color defColorHits1;
+	static const SDL_Color defColorHits2;
+	static const SDL_Color defColorHits3;
 	static constexpr float defWidth{ 60.f }, defHeight{ 20.f };
 	static constexpr float defVelocity{ 8.f };
 
@@ -215,9 +326,9 @@ public:
 
 	Brick(float mX, float mY)
 	{
-		shape.setPosition(mX, mY);
-		shape.setSize({ defWidth, defHeight });
-		shape.setOrigin(defWidth / 2.f, defHeight / 2.f);
+		setPosition(mX, mY);
+		setSize( defWidth, defHeight );
+		setOrigin(defWidth / 2.f, defHeight / 2.f);
 	}
 
 	void update() override
@@ -225,18 +336,21 @@ public:
 		// Let's alter the color of the brick depending on the
 		// required hits.
 		if (requiredHits == 1)
-			shape.setFillColor(defColorHits1);
+			setFillColor(defColorHits1);
 		else if (requiredHits == 2)
-			shape.setFillColor(defColorHits2);
+			setFillColor(defColorHits2);
 		else
-			shape.setFillColor(defColorHits3);
+			setFillColor(defColorHits3);
 	}
-	void draw(sf::RenderWindow& mTarget) override { mTarget.draw(shape); }
+	void draw(SDL_Renderer *renderer) override 
+	{ 
+		drawShape( renderer );
+	}
 };
 
-const sf::Color Brick::defColorHits1{ 255, 255, 0, 80 };
-const sf::Color Brick::defColorHits2{ 255, 255, 0, 170 };
-const sf::Color Brick::defColorHits3{ 255, 255, 0, 255 };
+const SDL_Color Brick::defColorHits1{ 255, 255, 0, 80 };
+const SDL_Color Brick::defColorHits2{ 255, 255, 0, 170 };
+const SDL_Color Brick::defColorHits3{ 255, 255, 0, 255 };
 
 template <typename T1, typename T2>
 bool isIntersecting(const T1& mA, const T2& mB) noexcept
@@ -251,7 +365,7 @@ void solvePaddleBallCollision(const Paddle& mPaddle, Ball& mBall) noexcept
 
 	mBall.velocity.y = -Ball::defVelocity;
 	mBall.velocity.x =
-		mBall.x() < mPaddle.x() ? -Ball::defVelocity : Ball::defVelocity;
+		mBall.x < mPaddle.x ? -Ball::defVelocity : Ball::defVelocity;
 }
 
 void solveBrickBallCollision(Brick& mBrick, Ball& mBall) noexcept
@@ -298,7 +412,7 @@ private:
 	static constexpr int brkStartColumn{ 1 }, brkStartRow{ 2 };
 	static constexpr float brkSpacing{ 3.f }, brkOffsetX{ 22.f };
 
-	sf::RenderWindow window{ { wndWidth, wndHeight }, "Arkanoid - 11" };
+	SDL_Window window{ { wndWidth, wndHeight }, "Arkanoid - 11" };
 	Manager manager;
 
 	// SFML offers an easy-to-use font and text class that we
@@ -454,154 +568,7 @@ int main()
 	return 0;
 }
 
-#else
-
-#include<string>
-#include <ostream>
-#include "SDL.h"
-#include "SDL_ttf.h"
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-/**
-* Log an SDL error with some error message to the output stream of our choice
-* @param os The output stream to write the message to
-* @param msg The error message to write, format will be msg error: SDL_GetError()
-*/
-void logSDLError( std::ostream &os, const std::string &msg )
-{
-	os << msg << " error: " << SDL_GetError() << std::endl;
-}
-
-/**
-* Render the message we want to display to a texture for drawing
-* @param message The message we want to display
-* @param fontFile The font we want to use to render the text
-* @param color The color we want the text to be
-* @param fontSize The size we want the font to be
-* @param renderer The renderer to load the texture in
-* @return An SDL_Texture containing the rendered message, or nullptr if something went wrong
-*/
-SDL_Texture* renderText( const std::string &message, const std::string &fontFile,
-	SDL_Color color, int fontSize, SDL_Renderer *renderer )
-{
-	//Open the font
-	TTF_Font *font = TTF_OpenFont( fontFile.c_str(), fontSize );
-	if (font == nullptr)
-	{
-		logSDLError( std::cout, "TTF_OpenFont" );
-		return nullptr;
-	}
-	//We need to first render to a surface as that's what TTF_RenderText
-	//returns, then load that surface into a texture
-	SDL_Surface *surf = TTF_RenderText_Blended( font, message.c_str(), color );
-	if (surf == nullptr)
-	{
-		TTF_CloseFont( font );
-		logSDLError( std::cout, "TTF_RenderText" );
-		return nullptr;
-	}
-	SDL_Texture *texture = SDL_CreateTextureFromSurface( renderer, surf );
-	if (texture == nullptr)
-	{
-		logSDLError( std::cout, "CreateTexture" );
-	}
-	//Clean up the surface and font
-	SDL_FreeSurface( surf );
-	TTF_CloseFont( font );
-	return texture;
-}
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at some destination rect
-* taking a clip of the texture if desired
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param dst The destination rectangle to render the texture to
-* @param clip The sub-section of the texture to draw (clipping rect)
-*		default of nullptr draws the entire texture
-*/
-void renderTexture( SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
-	SDL_Rect *clip = nullptr )
-{
-	SDL_RenderCopy( ren, tex, clip, &dst );
-}
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height and taking a clip of the texture if desired
-* If a clip is passed, the clip's width and height will be used instead of
-*	the texture's
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-* @param clip The sub-section of the texture to draw (clipping rect)
-*		default of nullptr draws the entire texture
-*/
-void renderTexture( SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
-	SDL_Rect *clip = nullptr )
-{
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	if (clip != nullptr)
-	{
-		dst.w = clip->w;
-		dst.h = clip->h;
-	}
-	else
-	{
-		SDL_QueryTexture( tex, NULL, NULL, &dst.w, &dst.h );
-	}
-	renderTexture( tex, ren, dst, clip );
-}
-
-void fill_circle( SDL_Renderer *renderer, int cx, int cy, int radius, Uint8 r, Uint8 g, Uint8 b, Uint8 a )
-{
-	// Note that there is more to altering the bitrate of this 
-	// method than just changing this value.  See how pixels are
-	// altered at the following web page for tips:
-	//   http://www.libsdl.org/intro.en/usingvideo.html
-	static const int BPP = 4;
-
-	//double ra = (double)radius;
-
-	for (double dy = 1; dy <= radius; dy += 1.0)
-	{
-		// This loop is unrolled a bit, only iterating through half of the
-		// height of the circle.  The result is used to draw a scan line and
-		// its mirror image below it.
-
-		// The following formula has been simplified from our original.  We
-		// are using half of the width of the circle because we are provided
-		// with a center and we need left/right coordinates.
-
-		double dx = floor( sqrt( (2.0 * radius * dy) - (dy * dy) ) );
-		int x = cx - dx;
-		SDL_SetRenderDrawColor( renderer, r, g, b, a );
-		SDL_RenderDrawLine( renderer, cx - dx, cy + dy - radius, cx + dx, cy + dy - radius );
-		SDL_RenderDrawLine( renderer, cx - dx, cy - dy + radius, cx + dx, cy - dy + radius );
-
-		// Grab a pointer to the left-most pixel for each half of the circle
-		/*Uint8 *target_pixel_a = (Uint8 *)surface->pixels + ((int)(cy + r - dy)) * surface->pitch + x * BPP;
-		Uint8 *target_pixel_b = (Uint8 *)surface->pixels + ((int)(cy - r + dy)) * surface->pitch + x * BPP;
-		for (; x <= cx + dx; x++)
-		{
-		*(Uint32 *)target_pixel_a = pixel;
-		*(Uint32 *)target_pixel_b = pixel;
-		target_pixel_a += BPP;
-		target_pixel_b += BPP;
-		}*/
-		/*
-		// sleep for debug
-		SDL_RenderPresent(gRenderer);
-		std::this_thread::sleep_for(std::chrono::milliseconds{ 100 });
-		*/
-	}
-}
-
+#if 0
 int main( int, char** )
 {
 	// init SDL
