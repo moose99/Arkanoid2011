abdba1c2166afe919169447e91dd5a225341558e
moose99 <6894921+moose99@users.noreply.github.com>
2018-07-20 22:05:57 -0400

    added input events and text/font usage

--- Arkanoid2011/main.cpp
+++ Arkanoid2011/main.cpp
@@ -458,7 +458,8 @@ int main()
 
 #include<string>
 #include <ostream>
-#include <SDL.h>
+#include "SDL.h"
+#include "SDL_ttf.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -473,17 +474,109 @@ void logSDLError( std::ostream &os, const std::string &msg )
 	os << msg << " error: " << SDL_GetError() << std::endl;
 }
 
+/**
+* Render the message we want to display to a texture for drawing
+* @param message The message we want to display
+* @param fontFile The font we want to use to render the text
+* @param color The color we want the text to be
+* @param fontSize The size we want the font to be
+* @param renderer The renderer to load the texture in
+* @return An SDL_Texture containing the rendered message, or nullptr if something went wrong
+*/
+SDL_Texture* renderText( const std::string &message, const std::string &fontFile,
+	SDL_Color color, int fontSize, SDL_Renderer *renderer )
+{
+	//Open the font
+	TTF_Font *font = TTF_OpenFont( fontFile.c_str(), fontSize );
+	if (font == nullptr)
+	{
+		logSDLError( std::cout, "TTF_OpenFont" );
+		return nullptr;
+	}
+	//We need to first render to a surface as that's what TTF_RenderText
+	//returns, then load that surface into a texture
+	SDL_Surface *surf = TTF_RenderText_Blended( font, message.c_str(), color );
+	if (surf == nullptr)
+	{
+		TTF_CloseFont( font );
+		logSDLError( std::cout, "TTF_RenderText" );
+		return nullptr;
+	}
+	SDL_Texture *texture = SDL_CreateTextureFromSurface( renderer, surf );
+	if (texture == nullptr)
+	{
+		logSDLError( std::cout, "CreateTexture" );
+	}
+	//Clean up the surface and font
+	SDL_FreeSurface( surf );
+	TTF_CloseFont( font );
+	return texture;
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at some destination rect
+* taking a clip of the texture if desired
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param dst The destination rectangle to render the texture to
+* @param clip The sub-section of the texture to draw (clipping rect)
+*		default of nullptr draws the entire texture
+*/
+void renderTexture( SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
+	SDL_Rect *clip = nullptr )
+{
+	SDL_RenderCopy( ren, tex, clip, &dst );
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+* the texture's width and height and taking a clip of the texture if desired
+* If a clip is passed, the clip's width and height will be used instead of
+*	the texture's
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param x The x coordinate to draw to
+* @param y The y coordinate to draw to
+* @param clip The sub-section of the texture to draw (clipping rect)
+*		default of nullptr draws the entire texture
+*/
+void renderTexture( SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
+	SDL_Rect *clip = nullptr )
+{
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	if (clip != nullptr)
+	{
+		dst.w = clip->w;
+		dst.h = clip->h;
+	}
+	else
+	{
+		SDL_QueryTexture( tex, NULL, NULL, &dst.w, &dst.h );
+	}
+	renderTexture( tex, ren, dst, clip );
+}
+
 int main( int, char** )
 {
-
-	// Create window and renderer
+	// init SDL
 	if (SDL_Init( SDL_INIT_EVERYTHING ) != 0)
 	{
 		logSDLError( std::cout, "SDL_Init" );
 		return 1;
 	}
 
-	SDL_Window *window = SDL_CreateWindow( "Lesson 2", 100, 100, SCREEN_WIDTH,
+	// init TTF system
+	if (TTF_Init() != 0)
+	{
+		logSDLError( std::cout, "TTF_Init" );
+		SDL_Quit();
+		return 1;
+	}
+
+	// Create window and renderer
+	SDL_Window *window = SDL_CreateWindow( "ARKANOID", 100, 100, SCREEN_WIDTH,
 		SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 	if (window == nullptr)
 	{
@@ -525,17 +618,51 @@ int main( int, char** )
 		return 1;
 	}
 
-	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-	for (int i = 0; i < 3; ++i)
+	SDL_Color color = { 0, 0, 255, 255 };
+	SDL_Texture *image = renderText( "TTF fonts are cool!", "calibri.ttf",
+		color, 16, renderer );
+	if (image == nullptr)
 	{
-		//First clear the renderer
+		SDL_DestroyRenderer( renderer );
+		SDL_DestroyWindow( window );
+		TTF_Quit();
+		SDL_Quit();
+		return 1;
+	}
+
+	//Get the texture w/h so we can center it in the screen
+	int iW, iH;
+	SDL_QueryTexture( image, NULL, NULL, &iW, &iH );
+	int x = SCREEN_WIDTH / 2 - iW / 2;
+	int y = SCREEN_HEIGHT / 2 - iH / 2;
+
+
+	//e is an SDL_Event variable we've declared before entering the main loop
+	SDL_Event e;
+	bool quit = false;
+	while (!quit)
+	{
+		while (SDL_PollEvent( &e ))
+		{
+			//If user closes the window, presses escape
+			if (e.type == SDL_QUIT ||
+				(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) )
+			{
+				quit = true;
+			}
+		}
+
+		/* Select the color for drawing. It is set to red here. */
+		SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+		//First clear the renderer with the draw color
 		SDL_RenderClear( renderer );
 		//Draw the texture
 		SDL_RenderCopy( renderer, tex, NULL, NULL );
+		//We can draw our message as we do any other texture, since it's been
+		//rendered to a texture
+		renderTexture( image, renderer, x, y );
 		//Update the screen
 		SDL_RenderPresent( renderer );
-		//Take a quick break after all that hard work
-		SDL_Delay( 1000 );
 	}
 
 	// cleanup
